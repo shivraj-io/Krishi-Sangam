4306650d9d20f7b443e4fb024a452dc6a5ee17db
Shivraj Singh Pipawad <104923059+shivraj-io@users.noreply.github.com>
2025-10-17 13:34:51 +0530

    Delete backend_remote_import directory

--- backend_remote_import/src/routes/cropRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { predictCrop } = require("../controllers/cropController");
-const router = express.Router();
-
-router.post("/predict", predictCrop);
-
-module.exports = router;
