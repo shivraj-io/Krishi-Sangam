9af78a9e3bd7683aa9be6bf8560af2cec435ea7a
Shivraj Singh Pipawad <104923059+shivraj-io@users.noreply.github.com>
2025-10-17 13:39:21 +0530

    Delete backend/backend_remote_import directory

--- backend/backend_remote_import/src/routes/yieldRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { predictYield } = require("../controllers/yieldController");
-const router = express.Router();
-
-router.post("/predict", predictYield);
-
-module.exports = router;
